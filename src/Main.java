c2e34bf5616f0ba77b87c840ea3da1382d8e598b
BrezinovaKaterina <144615454+BrezinovaKaterina@users.noreply.github.com>
2023-09-19 20:23:50 +0200

    Dokončený druhý úkol

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name="Jana Nová";
+        LocalDate datNar= LocalDate.of(2000,01,06);
+        String mesto="Tábor";
+        String SPZ="7A7 7777";
+        Double spotreba=(5.3);
+        String IP="192.168.48.39";
+        int contractId;
+        int total;
+        int prodejPrumer;
+        contractId=4;
+        total=24;
+        prodejPrumer=total/contractId;
+        System.out.println("Průměrné množství prodané mrkve je " +prodejPrumer+ " ks.");
     }
 }
+
